0c48a0d2f5b8cae5cab0316ada30a5a9189a53fd
Andromeus <andrew.li3056@gmail.com>
2026-01-05 14:45:56 -0500

    updated Player.cs to have all the code of PlayerController and intialized WeaponAnimationController

--- the-cursed-inn/Assets/Scripts/Player Scripts/Player.cs
+++ the-cursed-inn/Assets/Scripts/Player Scripts/Player.cs
@@ -1,6 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player
+public class Player : MonoBehaviour
 {
-    
+    private InputSystem_Actions controls;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    //private bool isJumping = false;
+
+    //animators
+    private Animator _WeaponAnimationController;
+
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    public LayerMask groundLayer;
+
+    private void Awake()
+    {
+        // Initialize the InputSystem_Actions() instance
+        controls = new InputSystem_Actions();
+
+        // Subscribe to the movement and jump actions
+        controls.Player.Move.performed += ctx => moveInput =
+          ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput =
+          Vector2.zero;
+
+        controls.Player.Jump.performed += ctx => Jump();
+        controls.Player.Attack.performed += ctx => Attack();
+
+    }
+
+    private void OnEnable()
+    {
+        // Enable the input controls
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input controls when the player object is disabled
+        controls.Disable();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Apply horizontal movement based on input
+        // (Original code): rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+    }
+
+    private void Jump()
+    {
+        //ORIGINAL CODE:
+        //(Useless because we don't jump in topdown, just included for testing purposes for now, should be cleaned up later)
+        // Check if the player is grounded before jumping
+        // if (IsGrounded())
+        //{
+        // rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        //}
+        Debug.Log("Space Pressed!");
+    }
+
+    private bool IsGrounded()
+    {
+        // Use a simple ground check to see if the player is touching 
+        // the ground
+        RaycastHit2D hit = Physics2D.Raycast
+          (transform.position, Vector2.down, 0.1f, groundLayer);
+        return hit.collider != null;
+    }
+
+    public void Attack()
+    {
+
+    }
+
 }
+
